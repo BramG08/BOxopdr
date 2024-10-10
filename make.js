dbf076822c4bfbc4d6b384574714d364b2ab9323
BramG08 <bgrippeling08@gmail.com>
2024-10-10 13:52:41 +0200

    first commit

--- make.js
+++
@@ -1,6 +0,0 @@
-document.body.classList.add('dark-mode');
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? 'Dark/Light' : 'Light/Dark';
-});
